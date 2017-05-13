d28dbfdbf8b8060fbf14c7e5eb902f35cb94b7f0
Eric McCarthy <eric@limulus.net>
2017-05-13 09:16:18 -0700

    Better defaults in help message

--- src/bin/cli.js
+++ src/bin/cli.js
@@ -4,22 +4,24 @@ import yargs from 'yargs'
 import rc from 'rc'
 import {basename as pathBasename} from 'path'
 
+const config = rc('lenticular')
+
 yargs
   .commandDir('cmds')
   .demandCommand()
   .options({
     'product-name': {
       desc: 'Name of your product. Short and unique',
-      default: pathBasename(process.cwd()),
+      default: config.productName || pathBasename(process.cwd()),
       group: 'Global Options:',
     },
     'build-region': {
       desc: `Region for product's build pipeline`,
-      default: 'us-west-2',
+      default: config.buildRegion || 'us-west-2',
       group: 'Global Options:',
     },
   })
   .group('help', 'Global Options:')
-  .config(rc('lenticular'))
+  .config(config)
   .help()
   .argv

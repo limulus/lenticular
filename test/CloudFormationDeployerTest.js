ad370ef53316693e839383f6a2aa95be904e4849
Eric McCarthy <eric@limulus.net>
2017-05-28 11:24:44 -0700

    Removed unnecessary stub

--- test/CloudFormationDeployerTest.js
+++ test/CloudFormationDeployerTest.js
@@ -16,30 +16,6 @@ describe(`CloudFormationDeployer`, () => {
   }
 
   beforeEach(() => {
-    aws.stub('CloudFormation', 'describeStackEvents').yieldsAsync(null, {
-      StackEvents: [
-        {
-          StackId: 'stack0',
-          EventId: '1',
-          StackName: 'some-stack',
-          LogicalResourceId: 'SomeBucket',
-          PhysicalResourceId: 'some-bucket',
-          ResourceType: 'AWS::S3::Bucket',
-          Timestamp: new Date(),
-          ResourceStatus: 'CREATE_COMPLETE',
-        },
-        {
-          StackId: 'stack0',
-          EventId: '2',
-          StackName: 'some-stack',
-          Timestamp: new Date(),
-          ResourceType: 'AWS::CloudFormation::Stack',
-          ResourceStatus: 'UPDATE_COMPLETE'
-        }
-      ],
-      NextToken: null
-    })
-
     deployer = new CFDeployer(config)
   })
 

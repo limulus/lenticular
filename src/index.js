03985f4bfc9a84dbe63cc15b8c2b84feb1f5f3db
Eric McCarthy <eric@limulus.net>
2017-08-09 15:34:01 -0700

    Export LenticularYamlDoc class

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ export {default as InitFileWriter} from './lib/InitFileWriter.js'
 export {default as ArtifactGenerator} from './lib/ArtifactGenerator.js'
 export {default as SecretsManager} from './lib/SecretsManager.js'
 export {default as CloudFormationDeployer} from './lib/CloudFormationDeployer.js'
-export {convertLenticularYamlToCloudFormationYaml} from './lib/LenticularYamlDoc.js'
+
+export {
+  default as LenticularYamlDoc,
+  convertLenticularYamlToCloudFormationYaml,
+} from './lib/LenticularYamlDoc.js'
+
 export {configureAWS} from './lib/Configurable.js'
 export {default as config} from './lib/config.js'
